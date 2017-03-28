044fc3196d70b0efbd123fed9053a2f6dd853e5e
Boris Mikhaylov <kaguxmail@gmail.com>
2017-03-28 17:35:04 +0200

    use APP_REVISION instead of appsignal api

--- hooks/rancher-upgrade.sh
+++ hooks/rancher-upgrade.sh
@@ -5,10 +5,7 @@ echo "Upgrading service $RANCHER_SERVICE_NAME"
 service_id="$(rancher.py id_of $RANCHER_SERVICE_NAME)"
 echo "Service id is $service_id"
 echo "Executing upgrade command: rancher upgrade $service_id --start_first --auto_complete --imageUuid=\"docker:$1:$2\""
-rancher.py upgrade $service_id --start_first --auto_complete --imageUuid="docker:$1:$2"
+rancher.py upgrade $service_id --start_first --auto_complete --imageUuid="docker:$1:$2" --replace_env_name="APP_REVISION" --replace_env_value="$2"
 if [ -n "$SLACK_URL" ]; then
   slack.sh $SLACK_URL $SLACK_CHANNEL $SLACK_USER "Production has been updated to version \`$2\`"
-fi
-if [ -n "$APPSIGNAL_KEY" ]; then
-  appsignal.sh $APPSIGNAL_KEY $APPSIGNAL_NAME $APPSIGNAL_ENVIRONMENT $2 $APPSIGNAL_REPO $3
 fi
