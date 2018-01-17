9a118ca2a3da9d3fa28ea0337618219caac1e992
Juan Peri <eternoperegrino@gmail.com>
2018-01-17 17:30:09 +0100

    use start_first=True as it seems to be the right parameter

--- hooks/rancher-upgrade.sh
+++ hooks/rancher-upgrade.sh
@@ -7,8 +7,8 @@ echo "Service id is $service_id"
 if [ -n "$SLACK_URL" ]; then
   slack.sh $SLACK_URL $SLACK_CHANNEL $SLACK_USER "Production deploy of version \`$2\` is starting"
 fi
-echo "Executing upgrade command: rancher upgrade $service_id --start_first --auto_complete --imageUuid=\"docker:$1:$2\""
-rancher.py upgrade $service_id --start_first --auto_complete --imageUuid="docker:$1:$2" --replace_env_name="APP_REVISION" --replace_env_value="$2"
+echo "Executing upgrade command: rancher upgrade $service_id --start_first=True --complete_previous --auto_complete --imageUuid=\"docker:$1:$2\" --replace_env_name=\"APP_REVISION\" --replace_env_value=\"$2\""
+rancher.py upgrade $service_id --start_first=True --complete_previous --auto_complete --imageUuid="docker:$1:$2" --replace_env_name="APP_REVISION" --replace_env_value="$2"
 if [ -n "$SLACK_URL" ]; then
   slack.sh $SLACK_URL $SLACK_CHANNEL $SLACK_USER "Production has been updated to version \`$2\`"
 fi
